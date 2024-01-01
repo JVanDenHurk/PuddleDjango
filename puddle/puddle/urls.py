a8f89c855a15c6dfcb86f12315aa591591a51ac6
Justin Van Den Hurk <justin.t.hurk@gmail.com>
2024-01-01 21:43:27 +1030

    added item details

--- puddle/puddle/urls.py
+++ puddle/puddle/urls.py
@@ -14,13 +14,23 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 from core.views import index, contact
 
+######### don't do this. demo only!##########
+from django.conf import settings
+from django.conf.urls.static import static
+#############################################
+
 urlpatterns = [
     path('', index, name='index'),
+    path('items/', include('item.urls')),
     path('contact/', contact, name='contact'),
     path('admin/', admin.site.urls),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+  #################################################################
+  ##also don't add document root on production site only demo######
+
