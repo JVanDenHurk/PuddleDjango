eb8d45f18874c286cf032a003173ec48fda3fe8c
Justin Van Den Hurk <justin.t.hurk@gmail.com>
2024-01-03 12:06:06 +1030

    items not showing in browse

--- puddle/item/views.py
+++ puddle/item/views.py
@@ -1,8 +1,15 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, get_object_or_404, redirect
 
-from .form import NewItemForm
+from .forms import NewItemForm, EditItemForm
 from .models import Item
+
+def items(request):
+    items = Item.objects.filter(is_sold=False)
+
+    return render(request, 'item/items.html', {
+        'item': items,
+    })
 
 def detail(request, pk):
     item = get_object_or_404(Item, pk=pk)
@@ -25,11 +32,30 @@ def new(request):
 
             return redirect('item:detail', pk=item.id)
     else:
-        form = NewItemForm()
+        form = NewItemForm(request.POST, request.FILES)
 
     return render(request, 'item/form.html', {
         'form': form,
         'title': 'New Item',
+    })
+
+@login_required
+def edit(request, pk):
+    item = get_object_or_404(Item, pk=pk, created_by=request.user)
+    
+    if request.method == 'POST':
+        form = EditItemForm(request.POST, request.FILES, instance=item)
+
+        if form.is_valid():
+            form.save()
+
+            return redirect('item:detail', pk=item.id)
+    else:
+        form = EditItemForm(instance=item)
+
+    return render(request, 'item/form.html', {
+        'form': form,
+        'title': 'Edit Item',
     })
 
 @login_required
